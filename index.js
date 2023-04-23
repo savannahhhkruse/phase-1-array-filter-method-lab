a002b555f8b14505d430207352bd9eb080952e81
Savannah <skruse101@gmail.com>
2023-04-23 10:24:16 -0700

    array filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+const findMatching = (names, string) => {
+    return names.filter(names => names.toUpperCase() === string.toUpperCase());
+ }
+ 
+ const fuzzyMatch = (name, string) => {
+    return name.filter(name=> name.charAt(0) === string.charAt(0))
+ }
+ 
+ const matchName = (name, hometown)=> {
+     return name.filter(name => name.name === hometown);
+ }
